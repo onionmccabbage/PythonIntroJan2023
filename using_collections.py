02fba28bc76eefd716fca7600ddef8f2d2c91b34
onionmccabbage@live.com <onionmccabbage@live.com>
2023-01-23 14:04:54 +0000

    review exercise 1

--- using_collections.py
+++ using_collections.py
@@ -1,4 +1,6 @@
-from random import randint
+# from random inport * # not recommended style
+from random import randint, random, randbytes # consider poor style - usually import each individually
+import random # I now have the ENTIRE random library
 
 # we can easily populate collections
 odds = range(1,100,2) # all the odd numbers from 1 to 100
@@ -7,7 +9,7 @@ for item in odds:
     print(item)
 
 # we can make a random number then say if it is odd or even
-r = randint(0,100)
+r = random.randint(0,100) # use a member of the library
 if r in odds: # check to see if a value exists in a collection
     s = 'odd'
 else:
