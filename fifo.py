dea1d8468ab4aaf9e1c1c524191096dd114dd687
onionmccabbage@live.com <onionmccabbage@live.com>
2023-01-24 10:32:03 +0000

    before coffee day 2

--- fifo.py
+++ fifo.py
@@ -1,23 +1,30 @@
 # file input and file output (fifo)
-# we smetimes need more control over the way we write or read text files
+# we sometimes need more control over the way we write or read text files
 t = 'here is a large amount of text to be written to a log file take two'
 
 # if we want we can pass in arguments to the function
 def fileOutput(t): # here we define a function
-    fout = open('mylog.txt', 'at') # append text
-    size = len(t) # how many characters in our text string
-    offset = 0
-    chunk = 24 # here we choose how many characters to write in one go
-    # next we will write our text string out, 24 characters at a time
-    while True:
-        if offset > size:
-            fout.write('\n') # all the text has vbeen written, so terminate with a new line character
-            break # we're all done!
-        else:
-            # [start : stop-before]
-            fout.write( t[offset:offset+chunk] ) # we wtie only 24 characters at a time
-            offset += chunk # += means ad the value to the current value
-    fout.close() # tidy up
+    # this is a good example where we should use exception handling
+    try: # Python will try to execute this block
+        fout = open('mylog.txt', 'at') # append text
+        size = len(t) # how many characters in our text string
+        offset = 0
+        chunk = 24 # here we choose how many characters to write in one go
+        # next we will write our text string out, 24 characters at a time
+        while True:
+            if offset > size:
+                fout.write('\n') # all the text has vbeen written, so terminate with a new line character
+                break # we're all done!
+            else:
+                # [start : stop-before]
+                fout.write( t[offset:offset+chunk] ) # we wtie only 24 characters at a time
+                offset += chunk # += means ad the value to the current value
+        fout.close() # tidy up
+    except Exception as err: # if something goes wrong ten Python will jump to this block
+        print( f'There was a problem: {err}' )
+    # you can have maultiple except blocks each for different exceptions
+    finally: # this block always runs, whether there is an exception or not
+        pass # here we choose to do nothing
 
 
 def fileInput(): # here is another function
@@ -40,8 +47,3 @@ while True:
         fileInput() # invoke our file reader function
     else:
         fileOutput(t) # here we pass in the value of t we wish to use
-
-
-
-
-
