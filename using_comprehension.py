52ce7c631d116b25baef0991f9e55df8e351356d
onionmccabbage@live.com <onionmccabbage@live.com>
2023-01-25 15:56:58 +0000

    example comprehension

---
+++ using_comprehension.py
@@ -0,0 +1,6 @@
+# the word 'comprehension' is used in Python
+# here it foes NOT mean understanding
+# it means comprehensively dealing with every member of a collection
+
+# here is a list that will contain all the even numbers from 0 to 100
+even_l = [ num for num in range(0, 101) if num%2 == 0 ] # this is called list comprehension
